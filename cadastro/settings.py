0001171007459ed51fb2ebd0792786dac3bf5c97
Gabriel de Souza Ferreira <64338299+gbszf@users.noreply.github.com>
2022-10-06 09:11:26 -0300

    Update settings.py

--- cadastro/settings.py
+++ cadastro/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+SECRET_KEY = 'django-insecure-g!jm!4yfcsafezfl%54y%2lo!@*ri*_ruu4yy$f5s9mr9vnj()'
 
 
 
